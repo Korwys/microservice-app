cd5b60a9e4a21b9c115e50b6ee16c7d8406fe9a2
Daniil S <sidorenkodk@gmail.com>
2023-02-05 15:26:47 +0300

    add: db_init event which start on server startup and init tables in db and add test data

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
+import json
+import logging.config
+
 import uvicorn
 from fastapi import FastAPI
 
+from config.initial_db_data import init_tables_and_data
+from products.router import product_router
 
 app = FastAPI()
+
+config_file = open('./config/logger.json')
+logging.config.dictConfig(json.load(config_file))
+
+
+@app.on_event("startup")
+async def db_init() -> None:
+    await init_tables_and_data()
+
 
 @app.get('/')
 def server():
     return "Hello Danche"
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
+app.include_router(product_router, tags=['product'], prefix='/api/product')
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3001)
